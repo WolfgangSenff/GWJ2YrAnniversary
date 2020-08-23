44a3fc6637af9092926eea9c5d4695e4600178fc
Michael Nock <pokemaster103@gmail.com>
2020-08-23 21:46:18 +0200

    Clues (#24)
    * Add Clue scene.
    
    * Add basic clue scene.

---
+++ Objects/Clues/Clue.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+export (int) var sniffer_level
+
+signal thought_done
+export (Resource) var DialogRes
+const ThoughtBubble = preload("res://UI/ThoughtBubble.tscn")
+
+func _ready() -> void:
+    var _e = connect("body_entered", self, "_on_body_entered")
+    visible = SnifferPower.current_sniffer_level >= sniffer_level
+
+func _on_body_entered(body):
+    if SnifferPower.current_sniffer_level >= sniffer_level:
+        get_tree().paused = true
+        yield(DialogRes.init_dialog(body, self), "completed")
+        body.show_thought_bubble("Sniffer leveled up!")
+        SnifferPower.raise_sniffer_level()
+        get_tree().paused = false
+    
+func press_skip_text():
+    if current_thought_bubble:
+        current_thought_bubble.skip_pressed = true
+        
+var current_thought_bubble
+
+func show_thought_bubble(text : String) -> void:
+    var thought_bubble = ThoughtBubble.instance()
+    current_thought_bubble = thought_bubble
+    add_child(thought_bubble)
+    thought_bubble.load_text(text)
+    yield(thought_bubble, "text_done")
+    emit_signal("thought_done")
